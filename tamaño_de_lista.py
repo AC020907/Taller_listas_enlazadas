03d470546238dedc43f3820801018d34433a0163
AC020907 <acotes950@gmail.com>
2026-04-08 11:40:53 -0500

    Delete tamaño_de_lista.py

--- tamaño_de_lista.py
+++
@@ -1,8 +0,0 @@
-def tamañoLista(self,lista_enlazada):
-    current = lista_enlazada.head
-    contador = 0
-    while current is not None:
-        contador +=1
-        current.next
-    
-    return contador
