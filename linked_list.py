83efb45bf687e2cbe4db2801d415df6dea841e25
AC020907 <acotes950@gmail.com>
2026-04-08 11:38:30 -0500

    Implement method to reverse linked list
    Added a method to reverse a linked list.

--- linked_list.py
+++ linked_list.py
@@ -42,4 +42,13 @@ class LinkedList:
                     index.data= aux
                 index = index.next
             current = current.next
+        def invertirLista(self,lista_enlazada):
+            current = lista_enlazada.head
+            prev = None 
+            while current is not None:
+                next_node = current.next
+                current.next = prev
+                prev = current
+                current = next_node
+            self.head = prev
 
