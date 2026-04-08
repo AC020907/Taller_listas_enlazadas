fb63834e3c91dbf1a2f1e6c17aa3a019b1809df4
AC020907 <acotes950@gmail.com>
2026-04-08 11:39:35 -0500

    Implement tamañoLista method for linked list
    Add method to count the number of nodes in a linked list.

--- linked_list.py
+++ linked_list.py
@@ -51,4 +51,12 @@ class LinkedList:
                 prev = current
                 current = next_node
             self.head = prev
+        def tamañoLista(self,lista_enlazada):
+            current = lista_enlazada.head
+            contador = 0
+            while current is not None:
+                contador +=1
+                current.next
+            return contador
+
 
