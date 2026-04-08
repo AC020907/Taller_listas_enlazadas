0cb5877cf902fb3d9ec04810b066a4bd43530b60
mspedraza09 <mspedraza@uninorte.edu.co>
2026-04-08 16:28:35 +0000

    Implementación de busqueda, invertir y tamaño_lista

---
+++ tamaño_de_lista.py
@@ -0,0 +1,8 @@
+def tamañoLista(self,lista_enlazada):
+    current = lista_enlazada.head
+    contador = 0
+    while current is not None:
+        contador +=1
+        current.next
+    
+    return contador
