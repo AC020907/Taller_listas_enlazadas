4e6c609891115b5185e5e6bf1daee380498f0c5a
AC020907 <acotes950@gmail.com>
2026-04-08 11:40:36 -0500

    Delete busqueda.py

--- busqueda.py
+++
@@ -1,9 +0,0 @@
-def search(self, target):
-    current = self.head
-
-    while current is not None:
-        if current.data == target:
-            return True
-        current = current.next
-
-    return False
